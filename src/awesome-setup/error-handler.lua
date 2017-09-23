77e7be170a139d7dd4a206a7d99f886f8a63c5d8
Groboclown <matt@groboclown.net>
2017-09-23 00:16:26 +0000

    Add logging reference

---
+++ src/awesome-setup/error-handler.lua
@@ -0,0 +1,58 @@
+--
+-- Awesome startup and runtime error handlers.
+--
+-- Logs errors to the screen with "naughty", and to the log file.
+
+local mod = {
+    -- allows for access to the logger.
+    _config_module = nil;
+}
+
+
+-- Conditionally load "naughty" in case we've been loaded directly
+-- from the CLI.
+local nexist, naughty = pcall(require, "naughty")
+
+local function log_error(...)
+    if mod._config_module ~= nil and mod._config_module.log ~= nil then
+        local depth = mod._config_module.log._logger_stack_depth
+        mod._config_module.log._logger_stack_depth = 4
+        mod._config_module.log.error(...)
+        mod._config_module.log._logger_stack_depth = depth
+    end
+end
+
+
+-- Check if awesome encountered an error during startup and fell back to
+-- another config (This code will only ever execute for the fallback config)
+function mod.check_startup_errors()
+    if awesome.startup_errors then
+        log_error("AwesomeWM Startup Errors:", awesome.startup_errors)
+        naughty.notify{ preset = naughty.config.presets.critical;
+                     title = "Oops, there were errors during startup!";
+                     text = awesome.startup_errors }
+    end
+end
+
+-- Handle runtime errors after startup
+function mod.handle_runtime_errors()
+    local in_error = false
+    awesome.connect_signal("debug::error", function (err)
+        -- Make sure we don't go into an endless error loop
+        if in_error then return end
+        in_error = true
+        
+        log_error("AwesomeWM Runtime Error:", err)
+        naughty.notify({ preset = naughty.config.presets.critical,
+                         title = "Oops, an error happened!",
+                         text = tostring(err) })
+        in_error = false
+    end)
+end
+
+function mod.setup_error_handlers()
+    mod.check_startup_errors()
+    mod.handle_runtime_errors()
+end
+
+return mod
---
+++ src/awesome-setup/log.lua
@@ -0,0 +1,85 @@
+-- Simple logging library.
+
+-- Taken somewhat from https://github.com/rxi/log.lua
+-- which is Copyright (c) 2016 rxi
+-- and released under the MIT license.
+
+local tostring = tostring
+local ipairs = ipairs
+
+local levels = {
+    { name = "trace"; color = "\27[34m"; display = "[TRACE "; },
+    { name = "debug"; color = "\27[36m"; display = "[DEBUG "; },
+    { name = "info";  color = "\27[32m"; display = "[ INFO "; },
+    { name = "warn";  color = "\27[33m"; display = "[ WARN "; },
+    { name = "error"; color = "\27[31m"; display = "[ERROR "; },
+    { name = "fatal"; color = "\27[35m"; display = "[FATAL "; },
+}
+
+local log = {
+    logfile = nil;
+    console = false;
+    color = true;
+    level = "trace";
+    _logger_stack_depth = 2;
+    
+    -- TODO look at adding log rotation.
+}
+
+local function _round(x, inc)
+    inc = inc or 1
+    x = x / inc
+    return (x > 0 and math.floor(x + .5) or math.ceil(x - .5)) * inc
+end
+
+local function _tostring(...)
+    local t = {}
+    for i = 1, select('#', ...) do
+        local x = select(i, ...)
+        if type(x) == 'number' then
+            -- show just a limited value for the number
+            x = round(x, 0.001)
+        end
+        t[#t + 1] = tostring(x)
+    end
+    return table.concat(t, " ")
+end
+
+local level_index = {}
+
+for i,c in ipairs(levels) do
+    level_index[c.name] = i
+    log[c.name] = function(...)
+        if level_index[log.level] ~= nil or i < level_index[log.level] then
+            -- early out if log level hides this message
+            return
+        end
+        local msg = _tostring(...)
+        local info = debug.getinfo(log._logger_stack_depth, "Sl")
+        local src = info.short_src .. ';' .. info.currentline
+        local now = os.date("%H:%M:%S")
+        local nocolor = nil
+        
+        if log.console and log.color then
+            print(string.format("%s%s%s]\27[0m %s: %s"),
+                c.color, c.display, now, src, msg)
+        elseif log.console then
+            nocolor = string.format("%s%s] %s: %s",
+                c.display, now, src, msg)
+            print(nocolor)
+        end
+        if log.logfile then
+            if nocolor == nil then
+                nocolor = string.format("%s%s] %s: %s",
+                    c.display, now, src, msg)
+            end
+            local fp = io.open(log.logfile, 'a')
+            if fp ~= nil then
+                fp:write(nocolor)
+                fp:close()
+            end
+        end
+    end
+end
+
+return log
